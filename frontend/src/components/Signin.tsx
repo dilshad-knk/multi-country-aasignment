abde00077f29cecada33c988f6b71177e8789d42
dilshad-knk <dilshadali.knk@gmail.com>
2024-11-16 05:46:45 +0000

    credential

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -8,8 +8,8 @@ import toast from 'react-hot-toast';
 
 const Login = () => {
  
-  const [email, setEmail] = useState('asdf@gmail.com');
-  const [password, setPassword] = useState('asdf');
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const setUser = useSetRecoilState<any>(userState);
 
   const navigate =useNavigate()
