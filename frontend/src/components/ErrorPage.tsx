686175dd9e6932868b067a996446c98b27489e2c
dilshad-knk <dilshadali.knk@gmail.com>
2024-11-16 15:18:17 +0000

    u

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -1,7 +1,7 @@
 
 const ErrorPage = () => {
   return (
-    <div className='h-screen bg-red-800 flex items-center justify-center font-extrabold text-5xl'>Ooops ! </div>
+    <div className='h-screen bg-red-800 flex items-center justify-center font-extrabold text-5xl'>Oops ! </div>
   )
 }
 
